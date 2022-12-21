a3298575c33c92b6e7005983cf4ba3e6cb4db600
Yakubu Alhassan <yalhassanpro@gmail.com>
2022-12-21 14:12:08 +0100

    create API

---
+++ index.js
@@ -0,0 +1,18 @@
+// an express server, which will handle incoming spi requests and respond with json, it will use body parser as well as cors.
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+});
+
+app.listen(port, ()=>{
+    console.log(`App is running on port 4000`)
+});
